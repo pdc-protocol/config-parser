7cac361677fbaa45150d7685a55e29f7a656e2a1
Arnaud Biju-Duval <abijuduval@corp.free.fr>
2023-11-08 10:25:00 +0100

    refactor: suppress comments

--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,6 @@ export class PDCPConfigFile {
     return Object.keys(this.config.projects);
   }
 
-  // isPDCPConfig(obj: any | PDCPConfig): obj is PDCPConfig {
-  //   return;
-  // }
-  //YAML to JSON string ou fichier
   async parseFromFile(configFilePath: string) {
     await fs.promises
       .readFile(configFilePath, 'utf8')
@@ -30,42 +26,9 @@ export class PDCPConfigFile {
 
   async parseFromString(configString) {
     const doc = parseDocument(configString);
-    // console.log('doc');
     this.config = doc.toJS(); //any a la place de PDCP / 'class validator' ?
     if (this.config && !('projects' in this.config)) {
       throw new Error(`config has no projects`);
     }
   }
 }
-
-// export class PDCPConfigString {
-//   config: null | PDCPConfig = null;
-
-//   constructor(public configString: string) { }
-
-//   getProjectsNames(): Array<string> {
-//     if (this.config === null) {
-//       throw Error('Error: config is null');
-//     }
-//     return Object.keys(this.config.projects);
-//   }
-
-//   // isPDCPConfig(obj: any | PDCPConfig): obj is PDCPConfig {
-//   //   return;
-//   // }
-//   //YAML to JSON string ou fichier
-//   async parse() {
-//     await fs.promises
-//       .readFile(this.configString, 'utf8')
-//       .then((data) => {
-//         const doc = parseDocument(data);
-//         this.config = doc.toJS(); //any a la place de PDCP / 'class validator' ?
-//         if (this.config && !('projects' in this.config)) {
-//           throw new Error(`file: ${this.configString} has no projects`);
-//         }
-//       })
-//       .catch((err) => {
-//         throw new Error(err.message);
-//       });
-//   }
-// }
